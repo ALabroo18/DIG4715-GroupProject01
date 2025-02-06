787f9e46269b4a8e4c2879f2f69fad086a70364f
ALabroo18 <102484252+ALabroo18@users.noreply.github.com>
2025-02-06 09:57:33 -0500

    Fixed a small error where enemies would be treated like players to item objects

--- DIG4715_Proj01/Assets/Scripts/itemScript.cs
+++ DIG4715_Proj01/Assets/Scripts/itemScript.cs
@@ -20,39 +20,44 @@ public class itemScript : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other){
 
-         PlayerBehavior pB = other.GetComponent<PlayerBehavior>();
 
-        Debug.Log("Reached trigger");
-        if(this.gameObject.CompareTag("meal")){
-            other.GetComponent<PlayerBehavior>().ChangeLives(100);
-            Debug.Log("Got meal");
-            Destroy(this.gameObject);
+            if(other.gameObject.CompareTag("Player")){
+                PlayerBehavior pB = other.GetComponent<PlayerBehavior>();
 
-             pB.PlaySound(audioClip);
+            Debug.Log("Reached trigger");
+            if(this.gameObject.CompareTag("meal")){
+                other.GetComponent<PlayerBehavior>().ChangeLives(100);
+                Debug.Log("Got meal");
+                Destroy(this.gameObject);
 
+                pB.PlaySound(audioClip);
+
+            }
+            else if(this.gameObject.CompareTag("chest")){
+
+                Debug.Log("Got chest");
+                other.GetComponent<PlayerBehavior>().ChangeScore(500);  
+                Destroy(this.gameObject);
+                pB.PlaySound(audioClip);
+            }
+            else if(this.gameObject.CompareTag("potion")){
+                other.GetComponent<PlayerBehavior>().potionCount += 1;
+                Debug.Log("Got potion");
+                Destroy(this.gameObject);
+
+                pB.PlaySound(audioClip);
+            }
+            else if(this.gameObject.CompareTag("key")){
+                other.GetComponent<PlayerBehavior>().hasKey = true;
+                Debug.Log("Got key");
+                Destroy(this.gameObject);
+
+                pB.PlaySound(audioClip);
+
+            }
         }
-        else if(this.gameObject.CompareTag("chest")){
-            other.GetComponent<PlayerBehavior>().ChangeScore(500);
-            Debug.Log("Got chest");
-            Destroy(this.gameObject);
 
-            pB.PlaySound(audioClip);
-        }
-        else if(this.gameObject.CompareTag("potion")){
-            other.GetComponent<PlayerBehavior>().potionCount += 1;
-            Debug.Log("Got potion");
-            Destroy(this.gameObject);
-
-            pB.PlaySound(audioClip);
-        }
-        else if(this.gameObject.CompareTag("key")){
-            other.GetComponent<PlayerBehavior>().hasKey = true;
-            Debug.Log("Got key");
-            Destroy(this.gameObject);
-
-            pB.PlaySound(audioClip);
-
-        }
+       
     }
 
     private void OnCollisionEnter2D(Collision2D coll){
